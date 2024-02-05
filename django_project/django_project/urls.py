efc8d5dfb13bb17d8e79b3213ed34eb4cc5d1bfb
devlumba <ilovesylphy@gmail.com>
2024-02-05 22:12:14 +0300

    4th commit, part 9 finished, uploaded images for every profile

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -23,6 +23,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 
@@ -36,3 +38,5 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
